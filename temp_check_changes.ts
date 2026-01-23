b571e8636d96c447c98e7d13c28b7963ce5823dd
Vishnu <wrvishnu@gmail.com>
2026-01-23 19:18:33 +0800

    removed unwanted files

--- temp_check_changes.ts
+++
@@ -1,8 +0,0 @@
-import { spfi, SPFx } from "@pnp/sp";
-import "@pnp/sp/sites";
-import "@pnp/sp/webs";
-import { IChangeQuery } from "@pnp/sp/types";
-
-// Note: This is a pseudo-script to demonstrate what I would check. 
-// I can't actually run standalone TS against the live site without auth.
-// But I can check my existing Service code to see what library access I have.
